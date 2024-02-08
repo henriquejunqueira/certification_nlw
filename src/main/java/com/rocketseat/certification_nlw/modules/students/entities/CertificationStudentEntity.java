a22c8001a07270b38326fcc98b0b7e7e4ef269ac
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-08 14:34:22 -0300

    re-upando o projeto

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -1,8 +1,19 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +21,31 @@ import lombok.NoArgsConstructor;
 @Data // o lombok coloca os getters e setters automaticamente
 @AllArgsConstructor // cria um construtor com todos os atributos
 @NoArgsConstructor // cria um construtor vazio
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grade;
+    
+    @JoinColumn(name = "student_id") // JoinColumn é usado em conjunto com @OneToMany ou @ManyToOne para especificar a coluna de junção na tabela do banco de dados
+    private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
     List<AnswersCertificationsEntity> answersCertificationsEntities;
     
-
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,18 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +20,21 @@ import lombok.NoArgsConstructor;
 @Data // o lombok coloca os getters e setters automaticamente
 @AllArgsConstructor // cria um construtor com todos os atributos
 @NoArgsConstructor // cria um construtor vazio
+@Entity(name = "students") // indica que isso é uma tabela e que o nome é students
 public class StudentEntity {
     
+    @Id // indica o id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    // pra definir o nome da coluna é só colocar a propriedade nome="nome_coluna"
+    @Column(unique = true, nullable = false) // unique se o campo precisa ser único, nullable se ele pode ser nulo, length define no máximo 100 caracteres
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntities;
+    
+    @OneToMany(mappedBy = "studentEntity") // Define um relacionamento de um para muitos entre duas entidades. Nesse caso um estudante para muitas certificações
+    private List<CertificationStudentEntity> certificationStudentEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
