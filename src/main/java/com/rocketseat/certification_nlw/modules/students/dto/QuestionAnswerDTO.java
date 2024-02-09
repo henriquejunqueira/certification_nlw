7123172adadd762bf975279cc0f36a3db7ba9748
Henrique <henriquejunqueira15@gmail.com>
2024-02-09 12:49:29 -0300

    subindo atualizacao

--- src/main/java/com/rocketseat/certification_nlw/modules/students/dto/QuestionAnswerDTO.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/dto/QuestionAnswerDTO.java
@@ -1,15 +1,18 @@
 package com.rocketseat.certification_nlw.modules.students.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class VerifyIfHasCertificationDTO {
+@Builder
+public class QuestionAnswerDTO {
     
-    private String email;
-    private String technology;
+    private String questionID;
+    private String alternativeID;
+    private boolean isCorrect;
 
 }
