1f78a5e739ffd0007c1995f5d5da8f5f2589ce95
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-21 13:41:21 -0300

    Exceptions created, certification package created with the ranking controller and top 10 service

--- src/main/java/com/henriquejunqueira/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/henriquejunqueira/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,12 +1,16 @@
 package com.henriquejunqueira.certification_nlw.modules.students.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.henriquejunqueira.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
 import com.henriquejunqueira.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.henriquejunqueira.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import com.henriquejunqueira.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
 import com.henriquejunqueira.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 
 @RestController
@@ -15,6 +19,9 @@ public class StudentController {
     
     @Autowired
     private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    @Autowired
+    private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
 
     @PostMapping("/verifyIfHasCertification")
     public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO){
@@ -26,6 +33,16 @@ public class StudentController {
         }
 
         return "Usuário pode fazer a prova";
+    }
+
+    @PostMapping("/certification/answer")
+    public ResponseEntity<Object> certificationAnswer(@RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) throws Exception{
+        try{
+            var result = studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+            return ResponseEntity.ok().body(result);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/henriquejunqueira/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/henriquejunqueira/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -6,6 +6,10 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,7 +18,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.metamodel.Type.PersistenceType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -22,27 +28,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "certifications")
+@Builder
 public class CertificationStudentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
-    @JoinColumn(name = "student_id")
-    private UUID studentID;
     
     @Column(length = 100)
     private String technology;
-
+    
     @Column(length = 10)
     private int grade;
+
+    @Column(name = "student_id")
+    private UUID studentID;
 
     @ManyToOne
     @JoinColumn(name ="student_id", insertable = false, updatable = false)
     private StudentEntity studentEntity;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+    @JsonManagedReference
     List<AnswersCertificationsEntity> answersCertificationsEntities;
 
     @CreationTimestamp
